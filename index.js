aafb92f1d39e4f1d0426da234160b4800c75560e
niraj1234 <niraj.webtech@gmail.com>
2025-01-04 12:25:20 +0530

    Download API created for pdf file

--- index.js
+++ index.js
@@ -26,6 +26,48 @@ app.post('/generate-pdf' , (req,res) => {
         return res.status(400).json({success: false , message: "Data is missing"});
     }
 
-    const doc = new PDFDoc()
+    const doc = new PDFDoc();
+    const fileName = `output_${Date.now()}.pdf`;
+    const filePath = path.join(pdfDir , fileName);
+
+    const writeStream = fs.createWriteStream(filePath);
+    doc.pipe(writeStream);
+    doc.fontSize(40).text(title , {align: 'center'});
+    doc.moveDown();
+    doc.fontSize(20).text(name) ;
+    doc.moveDown();
+    doc.fontSize(100).fillColor('#cadcad').text(amount);
+    doc.end();
+    writeStream.on("finish" , () => {
+        res.status(200).json({
+            message: "PDF is generated",
+            donloadLink: `http://localhost:3000/download?file=${fileName}`
+        })
+    });
+
+    writeStream.on("error" , () => {
+        res.status(500).json({message: "Failed to generate PDF file"})
+    })
+    
+
+})
+
+
+// API for donload the PDF 
+app.get("/download" , (req,res) => {
+    const filename = req.query.file;
+    if(!filename){
+       return  res.status(400).json({error: "File name must be present"});
+    }
+    const filepath = path.join(pdfDir , filename);
+    if(!fs.existsSync(filepath)){
+        return  res.status(404).json({error: "File does not exist !! "});
+    }
+
+    res.download(filepath , filename , (err) => {
+        if(err){
+            res.status(500).json({ error :'Failed to download file'})
+        }
+    });
 
 })
